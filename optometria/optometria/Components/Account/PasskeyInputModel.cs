f660bbbc5e0f22b902ba6598b3ae526960ffd576
Aricita <Aricita@ARI>
2026-05-14 08:35:59 -0500

    solucion.blazor

---
+++ optometria/optometria/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace optometria.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
